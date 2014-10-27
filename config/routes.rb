b2d40fa46e3b38e9ed91c398dc827412abe29f51
Jason Law <jasonlaw89@gmail.com>
2014-10-27 15:09:33 +0800

    Create the basic authentication using devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :doctors
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -7,8 +8,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
-  get 'privacy' => 'welcome#privacy'
-  get 'terms'   => 'welcome#terms'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
